c257dd4aa54c45a8ba97600eb8dafafe74b5d440
tommy-snellman <tommy.sn95@gmail.com>
2020-06-25 06:37:41 +0300

    Updates

---
+++ precache-manifest.ae639bf05adb7374df3930f7bb43a356.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea5f75639ce8421bb0eb8bcb53a3e109",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "7034e665561c21edd95e",
+    "url": "/simple-todo-app/static/css/main.50bfb8cf.chunk.css"
+  },
+  {
+    "revision": "8871a20e58820cf5337b",
+    "url": "/simple-todo-app/static/js/2.848c2ccc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.848c2ccc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7034e665561c21edd95e",
+    "url": "/simple-todo-app/static/js/main.7a0bb0cf.chunk.js"
+  },
+  {
+    "revision": "191a6f8718200c3f4cc6",
+    "url": "/simple-todo-app/static/js/runtime-main.cc60dfff.js"
+  }
+]);
